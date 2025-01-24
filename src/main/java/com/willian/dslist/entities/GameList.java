9d0a485a82931c7f79b5d19fab92f95c6e7e4c35
Willian Simon <wilxinelo@gmail.com>
2025-01-23 22:25:49 -0300

    Domain model

---
+++ src/main/java/com/willian/dslist/entities/GameList.java
@@ -0,0 +1,68 @@
+package com.willian.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nameString;
+	
+	public GameList() {
+		
+	}
+
+	public GameList(long id, String nameString) {
+		
+		this.id = id;
+		this.nameString = nameString;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNameString() {
+		return nameString;
+	}
+
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameList other = (GameList) obj;
+		return id == other.id;
+	}
+	
+	
+	
+	
+	
+
+}
